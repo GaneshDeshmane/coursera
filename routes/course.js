e1b3566746e8f3a3b37d31d7feea0083cba4f742
GaneshDeshmane <skycode.bots@gmail.com>
2026-06-16 23:54:33 +0530

    step-2 : Added Router

---
+++ routes/course.js
@@ -0,0 +1,11 @@
+const {Router}=require('express')
+
+const CourseRouter = Router
+
+CourseRouter.post('/purchases',function(req,res){
+
+})
+
+module.exports = {
+    CourseRouter : CourseRouter
+}
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const {Router}=require('express')
+
+const UserRouter = Router
+
+
+UserRouter.post('/signup',function(req,res){
+
+})
+UserRouter.post('/signin',function(req,res){
+     
+
+})
+UserRouter.get('/purchases',function(req,res){
+    
+})
+module.exports = {
+    UserRouter : UserRouter
+}
